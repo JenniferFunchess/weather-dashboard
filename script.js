7d7617db4167bead77561602e85861113c83b540
Jennifer Funchess <jfunchess@ymail.com>
2020-10-13 00:52:26 -0400

    set up original HTML and linked query api link

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
   console.log("ready!");
+
+  var APIKey = "c4fae5edaad45a9b13c98adafcd08019";
+  var chosenCity = $("#city-search").val();
+  console.log(chosenCity);
+
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + chosenCity + APIKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
 });
