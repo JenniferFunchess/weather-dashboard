945d0972b6951bfab787b1c9d9d01e6a52d817f0
Jennifer Funchess <jfunchess@ymail.com>
2020-10-13 22:47:20 -0400

    hid the chosen container and show it when you click search

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   console.log("ready!");
+  $("#popUp").hide();
 
   var APIKey = "c4fae5edaad45a9b13c98adafcd08019";
   var chosenCity = $("#citySearch").val().trim();
@@ -10,12 +11,10 @@ $(document).ready(function () {
     a.addClass("list");
     a.attr("data-name", chosenCity);
     a.text(chosenCity);
-    $("#citySearch").append(a);
+    $("#pastList").append(a);
   }
 
-  $("#citySubmit").on("click", function (event) {
-    event.preventDefault();
-
+  function cityContainer() {
     var chosenCity = $("#citySearch").val().trim();
     console.log(chosenCity);
     var queryURL =
@@ -34,9 +33,9 @@ $(document).ready(function () {
       console.log(new Date());
       var date =
         today.getFullYear() +
-        "-" +
+        "/" +
         (today.getMonth() + 1) +
-        "-" +
+        "/" +
         today.getDate();
       console.log(date);
 
@@ -46,5 +45,66 @@ $(document).ready(function () {
       $("#windChosen").text("Wind Speed: " + response.wind.speed + " MPH");
       $("#uvChosen").text("UV Index: ");
     });
+  }
+
+  function futureForecast() {
+    var chosenCity = $("#citySearch").val().trim();
+    console.log(chosenCity + "2");
+    var queryURL2 =
+      "http://api.openweathermap.org/data/2.5/forecast/daily?q=" +
+      chosenCity +
+      "&units=imperial&cnt=5&appid=" +
+      APIKey;
+
+    $.ajax({
+      url: queryURL2,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      $("#date1").text(response.list[0].dt_txt);
+      $("#date2").text(response.list[1].dt_txt);
+      $("#date3").text(response.list[2].dt_txt);
+      $("#date4").text(response.list[3].dt_txt);
+      $("#date5").text(response.list[4].dt_txt);
+
+      $("#temp1").text("Temp: " + response.list[0].main.temp + "°F");
+      $("#temp2").text("Temp: " + response.list[1].main.temp + "°F");
+      $("#temp3").text("Temp: " + response.list[2].main.temp + "°F");
+      $("#temp4").text("Temp: " + response.list[3].main.temp + "°F");
+      $("#temp5").text("Temp: " + response.list[4].main.temp + "°F");
+
+      $("#humidity1").text("Humidity: " + response.list[4].main.temp + "%");
+      $("#humidity2").text("Humidity: " + response.list[4].main.temp + "%");
+      $("#humidity3").text("Humidity: " + response.list[4].main.temp + "%");
+      $("#humidity4").text("Humidity: " + response.list[4].main.temp + "%");
+      $("#humidity5").text("Humidity: " + response.list[4].main.temp + "%");
+    });
+  }
+
+  function uvIndex() {
+    var chosenCity = $("#citySearch").val().trim();
+    console.log(chosenCity + "3");
+    var queryURL3 =
+      "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+      lat +
+      "&lon=" +
+      long +
+      "&appid=" +
+      APIKey;
+
+    $.ajax({
+      url: queryURL3,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+    });
+  }
+
+  $("#citySubmit").on("click", function (event) {
+    event.preventDefault();
+    $("#popUp").show();
+    cityContainer();
+    futureForecast();
+    renderList();
   });
 });
