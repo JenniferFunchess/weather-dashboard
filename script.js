123d10d37d54bd921fe4f82612598ce0286260e5
Jennifer Funchess <jfunchess@ymail.com>
2020-10-14 00:00:55 -0400

    added a for loop for future searches

--- script.js
+++ script.js
@@ -7,16 +7,21 @@ $(document).ready(function () {
 
   function renderList() {
     $("#citySearch").empty();
-    var a = $("<ul>");
-    a.addClass("list");
-    a.attr("data-name", chosenCity);
-    a.text(chosenCity);
-    $("#pastList").append(a);
+
+    for (var i = 0; i < chosenCity.length; i++) {
+      var a = $("<ul>");
+      a.addClass("list");
+      a.attr("data-name", chosenCity);
+      a.text(chosenCity);
+      $("#pastList").append(a);
+      console.log(pastList);
+    }
   }
 
   function cityContainer() {
     var chosenCity = $("#citySearch").val().trim();
     console.log(chosenCity);
+
     var queryURL =
       "http://api.openweathermap.org/data/2.5/weather?q=" +
       chosenCity +
@@ -44,59 +49,78 @@ $(document).ready(function () {
       $("#humidityChosen").text("Humidity: " + response.main.humidity + "%");
       $("#windChosen").text("Wind Speed: " + response.wind.speed + " MPH");
       $("#uvChosen").text("UV Index: ");
-    });
-  }
 
-  function futureForecast() {
-    var chosenCity = $("#citySearch").val().trim();
-    console.log(chosenCity + "2");
-    var queryURL2 =
-      "http://api.openweathermap.org/data/2.5/forecast/daily?q=" +
-      chosenCity +
-      "&units=imperial&cnt=5&appid=" +
-      APIKey;
+      var long = response.coord.lon;
+      var lat = response.coord.lat;
+      console.log("lat&long" + lat, long);
+      var queryURL3 =
+        "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+        lat +
+        "&lon=" +
+        long +
+        "&appid=" +
+        APIKey;
 
-    $.ajax({
-      url: queryURL2,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
-      $("#date1").text(response.list[0].dt_txt);
-      $("#date2").text(response.list[1].dt_txt);
-      $("#date3").text(response.list[2].dt_txt);
-      $("#date4").text(response.list[3].dt_txt);
-      $("#date5").text(response.list[4].dt_txt);
+      $.ajax({
+        url: queryURL3,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
 
-      $("#temp1").text("Temp: " + response.list[0].main.temp + "°F");
-      $("#temp2").text("Temp: " + response.list[1].main.temp + "°F");
-      $("#temp3").text("Temp: " + response.list[2].main.temp + "°F");
-      $("#temp4").text("Temp: " + response.list[3].main.temp + "°F");
-      $("#temp5").text("Temp: " + response.list[4].main.temp + "°F");
+        if (response.value <= 2) {
+          $("#uvChosen").attr("style", "background-color: green");
+          $("#uvChosen").css("color", "white");
+          $("#uvChosen").text("UV Index: " + response.value);
+        } else if (response.value >= 2 && response.value <= 5) {
+          $("#uvChosen").attr("style", "background-color: yellow");
+          $("#uvChosen").css("color", "white");
+          $("#uvChosen").text("UV Index: " + response.value);
+        } else if (response.value >= 6 && response.value <= 7) {
+          $("#uvChosen").attr("style", "background-color: orange");
+          $("#uvChosen").css("color", "white");
+          $("#uvChosen").text("UV Index: " + response.value);
+        } else if (response.value >= 8 && response.value <= 10) {
+          $("#uvChosen").attr("style", "background-color: red");
+          $("#uvChosen").css("color", "white");
+          $("#uvChosen").text("UV Index: " + response.value);
+        } else {
+          $("#uvChosen").attr("style", "background-color: purple");
+          $("#uvChosen").css("color", "white");
+          $("#uvChosen").text("UV Index: " + response.value);
+        }
+      });
 
-      $("#humidity1").text("Humidity: " + response.list[4].main.temp + "%");
-      $("#humidity2").text("Humidity: " + response.list[4].main.temp + "%");
-      $("#humidity3").text("Humidity: " + response.list[4].main.temp + "%");
-      $("#humidity4").text("Humidity: " + response.list[4].main.temp + "%");
-      $("#humidity5").text("Humidity: " + response.list[4].main.temp + "%");
-    });
-  }
+      var queryURL2 =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        lat +
+        "&lon=" +
+        long +
+        "&exclude=daily&cnt=5&units=imperial&appid=" +
+        APIKey;
 
-  function uvIndex() {
-    var chosenCity = $("#citySearch").val().trim();
-    console.log(chosenCity + "3");
-    var queryURL3 =
-      "http://api.openweathermap.org/data/2.5/uvi?lat=" +
-      lat +
-      "&lon=" +
-      long +
-      "&appid=" +
-      APIKey;
+      $.ajax({
+        url: queryURL2,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+        $("#date1").text(response.daily[0].dt);
+        $("#date2").text(response.daily[1].dt);
+        $("#date3").text(response.daily[2].dt);
+        $("#date4").text(response.daily[3].dt);
+        $("#date5").text(response.daily[4].dt);
 
-    $.ajax({
-      url: queryURL3,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
+        $("#temp1").text("Temp: " + response.daily[0].temp.day + "°F");
+        $("#temp2").text("Temp: " + response.daily[1].temp.day + "°F");
+        $("#temp3").text("Temp: " + response.daily[2].temp.day + "°F");
+        $("#temp4").text("Temp: " + response.daily[3].temp.day + "°F");
+        $("#temp5").text("Temp: " + response.daily[4].temp.day + "°F");
+
+        $("#humidity1").text("Humidity: " + response.daily[0].humidity + "%");
+        $("#humidity2").text("Humidity: " + response.daily[1].humidity + "%");
+        $("#humidity3").text("Humidity: " + response.daily[2].humidity + "%");
+        $("#humidity4").text("Humidity: " + response.daily[3].humidity + "%");
+        $("#humidity5").text("Humidity: " + response.daily[4].humidity + "%");
+      });
     });
   }
 
@@ -104,7 +128,6 @@ $(document).ready(function () {
     event.preventDefault();
     $("#popUp").show();
     cityContainer();
-    futureForecast();
     renderList();
   });
 });
