d3d090f20809fbb8b8cc881e8c2f91cf92527504
Jennifer Funchess <jfunchess@ymail.com>
2020-10-13 21:19:03 -0400

    linked the ajax function to the current weather container

--- script.js
+++ script.js
@@ -2,41 +2,49 @@ $(document).ready(function () {
   console.log("ready!");
 
   var APIKey = "c4fae5edaad45a9b13c98adafcd08019";
-  var chosenCity = $("#city-search").val();
-  console.log(chosenCity);
+  var chosenCity = $("#citySearch").val().trim();
 
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" + chosenCity + APIKey;
-  
-    function renderList() {
-        $("#city-search").empty();
-          var a = $("<ul>");
-          a.addClass("list");
-          a.attr("data-name", chosenCity);
-          a.text(chosenCity);
-          $("#city-search").append(a);
-        }
-  
-    $("#citySubmit").on("click", function (event) {
-        renderList();
-        
+  function renderList() {
+    $("#citySearch").empty();
+    var a = $("<ul>");
+    a.addClass("list");
+    a.attr("data-name", chosenCity);
+    a.text(chosenCity);
+    $("#citySearch").append(a);
+  }
+
+  $("#citySubmit").on("click", function (event) {
+    event.preventDefault();
+
+    var chosenCity = $("#citySearch").val().trim();
+    console.log(chosenCity);
+    var queryURL =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      chosenCity +
+      "&units=imperial&appid=" +
+      APIKey;
+
     $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
 
-    var currentTempF = (response.main.temp - 273.15) * 1.8 + 32;
+      var today = new Date();
+      console.log(new Date());
+      var date =
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate();
+      console.log(date);
 
-
-
-    $("#cityChosen").text(response.name + currentdate);
-    $("#tempChosen").text(currentTempF.toFixed(2));
-    $("#humidityChosen").text("Humidity: " + response.main.humidity);
-    $("#windChosen").text("Wind Speed: " + response.wind.speed);
-    $("#uvChosen").text("UV Index: " + );
-
-  });}
-
-
+      $("#cityChosen").text(response.name + "(" + date + ")");
+      $("#tempChosen").text("Temperature: " + response.main.temp + "°F");
+      $("#humidityChosen").text("Humidity: " + response.main.humidity + "%");
+      $("#windChosen").text("Wind Speed: " + response.wind.speed + " MPH");
+      $("#uvChosen").text("UV Index: ");
+    });
+  });
 });
